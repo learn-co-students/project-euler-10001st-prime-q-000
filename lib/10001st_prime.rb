96efae01e981d89d6326d4c12df1b8f3d1cd7bf6
uncledoofy156 <uncledoofy156@aim.com>
2015-11-18 20:02:23 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+  primes = []
+  i = 2
+  while primes.length < num
+    primes << i if prime?(i)
+    i += 1
+  end
+  primes[-1]
+end
+
+def prime?(num)
+  if num < 2
+    return false
+  end
+  i = 2
+  lim = num
+  while i < lim
+    if num % i == 0
+      return false
+    end
+    i += 1
+    lim = num / i + 1
+  end
+  return true
+end
