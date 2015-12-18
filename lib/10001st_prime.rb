063e2a083ea121092444f9b54383a9edf16eb027
azheng249 <andy.zheng249@gmail.com>
2015-12-18 21:54:58 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+
+# Using Sieve of Eratostheses algorithm to gather primes numbers efficiently
+# Source of code: http://rosettacode.org/wiki/Sieve_of_Eratosthenes#Ruby
+require_relative 'eratosthenes'
+
+# Returns the nth prime number up to 1,000,000th prime number(15,485,863)
+# Calculating until the 1,000,000th prime number may cause lag. Average runtime for program is 1.4 secs.
+def prime_number_for(nth_element)
+  number = 15485863
+  return "Cannot calculate that prime number." if nth_element <= 0 || nth_element > 1000000
+  primes = eratosthenes(number)
+
+  return primes[nth_element - 1]
+
+end
+
+prime_number_for(1)
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,12 @@
 # Implement your object-oriented solution here!
+
+require_relative '10001st_prime'
+
+# WARNING! TAKES AT LEAST 1.4 SECS TO INITALIZE PRIME each time
+#   - Program calculates up to the millionth prime number and takes a while to run
+class Prime
+  attr_accessor :number
+  def initialize(nth_element)
+    @number = prime_number_for(nth_element)
+  end
+end
