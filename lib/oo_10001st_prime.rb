f9a76df66843e8e418b421bb72b06e25a3d4d3a3
bradleyoashburn <bradleyoashburn@gmail.com>
2016-02-29 15:38:06 -1000

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class Prime
+
+  def initialize(number)
+    @number = number
+    @prime_array = []
+    @counter = 2
+  end
+
+  def is_prime?
+    Math.sqrt(@counter).floor.downto(2).each {|i| return false if @counter % i == 0}
+    return true
+  end
+
+  def number
+    while @prime_array.length < @number
+      if is_prime?
+        @prime_array.push(@counter)
+      end
+      @counter += 1
+    end
+    return @prime_array[@number-1]
+  end
+
+end
