c3e235dec6d57bb093f964f7fdbbb0f15f3054b2
bmolina-nyc <bruce.d.molina@gmail.com>
2016-03-01 22:57:36 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,33 @@
 # Implement your procedural solution here!
+
+
+
+def is_prime?(num)
+  2.upto(num-1).each { |test| return false if num % test == 0 }
+  true
+end
+
+
+
+def prime_number_for(amount)
+  primes = [] 
+  number = 2
+
+
+  while  true 
+    if is_prime?(number)
+      primes << number 
+        if primes.count == amount
+          return primes.last
+        end
+    end
+      
+    if number.even?
+      number += 1 
+    else
+      number += 2
+    end
+
+  end
+
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,39 @@
 # Implement your object-oriented solution here!
+
+
+class Prime
+
+  attr_accessor :index, :number, :result 
+
+  def initialize(prime)
+    @prime = prime 
+    @index = 2
+    @result = [] 
+  end
+
+  def prime?(number)
+    2.upto(number-1).each { |test| return false if number % test == 0 }
+    true
+  end
+  
+  def number
+      while true 
+        if prime?(index)
+          result << index
+          if result.count == @prime
+            return result.last 
+          end
+        end
+
+        if @index.even?
+          @index += 1
+        else
+          @index += 2
+        end
+
+      end
+
+  end
+
+end
+
