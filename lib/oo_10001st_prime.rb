7612965c1480033f0b7f463e5e492330f4538e08
einarstensson <einar.stensson@gmail.com>
2016-03-21 22:21:19 -0400

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,38 @@
-# Implement your object-oriented solution here!
+class Prime
+  attr_reader :number
+
+  def initialize(prime_count)
+    @prime_count = prime_count
+    @prime_candidate = 1
+    @number = prime_number_for
+  end
+
+  def prime_number_for
+    #Deal with the only even prime number: 2
+    return 2 if @prime_count == 1
+    @prime_count -= 1
+
+    @prime_candidate = 1
+
+    until @prime_count == 0
+      @prime_candidate += 2
+      @prime_count -= 1 if is_prime?
+    end
+
+    @prime_candidate
+  end
+
+  def is_prime?
+    return true if @prime_candidate == 2
+    return false if @prime_candidate % 2 == 0 || @prime_candidate == 1
+
+    divisor = 3
+
+    while divisor <= Math.sqrt(@prime_candidate)
+      return false if @prime_candidate % divisor == 0
+      divisor += 2
+    end
+
+    true
+  end
+end
