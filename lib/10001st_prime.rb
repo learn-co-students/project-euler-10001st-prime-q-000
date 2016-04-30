b53d24091c7ba958f44649487d4cbac5917ec0fa
thedailyephemera <thedailyephemera@gmail.com>
2016-04-30 16:20:09 -0400

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+def prime_check(num)
+	(2..num-1).each {|count| return false if num % count == 0}
+	return true
+end
+
+def prime_number_for(index_limit)
+	pieces = [2]
+	iter = 3
+	while pieces.count < index_limit
+		if prime_check(iter)
+			pieces << iter
+		end
+		iter += 2
+	end
+	pieces[-1]
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,21 @@
 # Implement your object-oriented solution here!
+class Prime
+	def initialize(index_limit)
+		@index_limit = index_limit
+	end
+	def prime_check(num)
+		(2..num-1).each {|count| return false if num % count == 0}
+		return true
+	end
+	def number
+		pieces = [2]
+		iter = 3
+		while pieces.count < @index_limit
+			if prime_check(iter)
+				pieces << iter
+			end
+			iter += 2
+		end
+		pieces[-1]
+	end
+end
