2f2024d84e0b5247851962974c182b7118f27c9b
John Anthony Rivera <askinganthony@gmail.com>
2016-05-15 23:13:49 -0700

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,6 @@
 # Implement your procedural solution here!
+require 'prime'
+
+def prime_number_for(nth)
+  (Prime.first nth)[-1] # Is this cheating? Next time I'll use the Sieve of Eratosthenes
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,11 @@
 # Implement your object-oriented solution here!
+class Prime
+  def initialize(nth_element)
+    @nth_element = nth_element # Felt like doing something different
+  end
+
+  def number
+    (Prime.first @nth_element)[-1]
+  end
+
+end
