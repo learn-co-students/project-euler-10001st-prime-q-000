715c85c70b630b01e9b148500e10bd2a2b059fe6
uu <giladbarkan@live.com>
2016-06-08 20:41:43 -0400

    passed tests

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,39 @@
 # Implement your procedural solution here!
+def prime_number_for(k)
+  # sieve of eratosthenes
+
+  n = 150000
+  s = Array.new(n).fill(true,2)
+
+  p = 2
+  sqrt = Math.sqrt(n).floor
+
+  while p < sqrt
+    if !s[p]
+      p += 1
+      next
+    end
+      
+    j=p*p
+    
+    until j >= n
+      s[j] = false
+      j += p
+    end
+    
+    p += 1
+  end
+
+  count = 0
+  i = 2
+
+  while count < k
+    if s[i] == true
+      count += 1
+    end
+    
+    i += 1
+  end
+
+  return i-1
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,46 @@
 # Implement your object-oriented solution here!
+class Prime
+  def initialize(k)
+    @k = k
+  end
+
+  def number
+    # sieve of eratosthenes
+
+    n = 150000
+    s = Array.new(n).fill(true,2)
+
+    p = 2
+    sqrt = Math.sqrt(n).floor
+
+    while p < sqrt
+      if !s[p]
+        p += 1
+        next
+      end
+        
+      j=p*p
+      
+      until j >= n
+        s[j] = false
+        j += p
+      end
+      
+      p += 1
+    end
+
+    count = 0
+    i = 2
+
+    while count < @k
+      if s[i] == true
+        count += 1
+      end
+      
+      i += 1
+    end
+
+    return i-1
+  end
+
+end
