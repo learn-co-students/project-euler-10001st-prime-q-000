b421a89d91e0d23f97535dfdb27b2665a6d51037
Pamela Alvarez <p.stacey.a@gmail.com>
2016-07-14 18:54:44 +0000

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+
+
+class Prime
+    def initialize(nth_element)
+      @nth_element =  (nth_element)
+    end
+
+    def number
+     current = 3
+     primes = [2]
+
+     while primes.size < @nth_element
+       primes << current if !(2..(current**0.5)).any?{ |p| current % p == 0 }
+       current += 2
+     end
+       primes.last
+    end
+end
