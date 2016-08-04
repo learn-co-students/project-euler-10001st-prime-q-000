67076d96d9d6e9f25cb832f5fadc752ab2d1d12e
Eoghan Leddy <eleddy521@gmail.com>
2016-08-04 19:29:55 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,53 @@
 # Implement your procedural solution here!
+FIRST_10 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+def prime_number_for(limit)
+  prime_number_array = fill_primes(limit)
+  prime_number_array[limit - 1]
+end
+
+def fill_primes(limit)
+
+  prime_nums = []
+  prime_nums = FIRST_10.each { |x| prime_nums << x }
+  counter = prime_nums[-1] + 2
+  until (prime_nums.length >= limit)
+
+    if(if_prime(counter))
+      prime_nums << counter
+      counter += 2
+    else
+      counter += 2
+    end
+
+  end
+
+  prime_nums
+
+end
+
+def if_prime(num_check)
+
+  if( FIRST_10.any? { |x| num_check % x == 0} )
+    return false
+  else
+    den_arr = fill_array(num_check)
+    if( den_arr.any? { |x| num_check % x == 0} )
+      return false
+    else
+      return true
+    end
+  end
+
+end
+
+def fill_array(limit)
+
+  counter = 3
+  denom_arr = [2]
+  until counter  > Math.sqrt(limit)
+    denom_arr << counter
+    counter += 2
+  end
+  denom_arr
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,68 @@
 # Implement your object-oriented solution here!
+class Prime
+
+  FIRST_10 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+
+  def initialize(index)
+
+    @index = index
+    @prime_nums = []
+    @prime_nums = FIRST_10.each { |x| @prime_nums << x }
+
+  end
+
+  def fill_array(limit)
+
+    if(@prime_nums.any? { |x| limit % x == 0 } )
+      @prime_nums
+    else
+      arr_counter = @prime_nums[-1] + 2
+      until arr_counter > Math.sqrt(limit)
+        @prime_nums << arr_counter
+        arr_counter += 2
+      end
+    end
+    @prime_nums = @prime_nums.uniq
+
+  end
+
+  def if_prime(num_check)
+
+    if( @prime_nums.any? { |x| num_check % x == 0} )
+      return false
+    else
+      fill_array(num_check)
+      if( @prime_nums.any? { |x| num_check % x == 0 } )
+        return false
+      else
+        return true
+      end
+
+    end
+
+  end
+
+  def fill_primes
+
+    counter = @prime_nums[-1] + 2
+    until (@prime_nums.length >= @index)
+
+      if(if_prime(counter))
+        @prime_nums << counter
+        counter += 2
+      else
+        counter += 2
+      end
+
+    end
+
+  end
+
+  def number
+
+    fill_primes
+    @prime_nums[@index - 1]
+
+  end
+
+end
