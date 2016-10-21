8fb3ea0ee4db5707114a4c2133d1677c524d0a91
Sean Koo <koo.sean@gmail.com>
2016-10-21 02:37:48 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,25 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+  primes = [2]
+  starter = 3
+  while primes.length < num
+    if prime?(starter)
+      primes.push(starter)
+    end
+    starter+=1
+  end
+  return primes.last
+end
+
+def prime?(n)
+  i = 2
+  while i < n
+    if n%i==0
+      return false
+      break
+    else
+      i += 1
+    end
+  end
+  return true
+end
