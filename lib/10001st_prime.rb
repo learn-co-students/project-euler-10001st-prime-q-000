afcd1ed65ab1278f21b7e88c2259d99b65a24ef9
Robert Hitt <robhitt@gmail.com>
2016-10-23 18:41:54 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,33 @@
 # Implement your procedural solution here!
+def prime_number_for(nth_element)
+	i = 0
+	prime_num = 0
+	prime_count = 0
+
+	loop do
+		while prime_count <= nth_element
+			if is_prime?(i)
+				prime_num = i
+				prime_count += 1
+				if prime_count == nth_element
+					return prime_num
+				end
+			end
+			i += 1
+		end
+	end
+end
+
+def is_prime?(nth_element)
+	x = 2
+
+	return false if nth_element < 2
+
+	while x < nth_element
+		if (nth_element % x == 0)
+			return false
+		end
+		x += 1
+	end
+	return true
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,40 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  def initialize(nth_element)
+    @nth_element = nth_element
+  end
+
+  def number
+  	i = 0
+  	prime_num = 0
+  	prime_count = 0
+
+  	loop do
+  		while prime_count <= @nth_element
+  			if is_prime?(i)
+  				prime_num = i
+  				prime_count += 1
+  				if prime_count == @nth_element
+  					return prime_num
+  				end
+  			end
+  			i += 1
+  		end
+  	end
+  end
+
+  def is_prime?(num)
+  	x = 2
+
+  	return false if num < 2
+
+  	while x < num
+  		if (num % x == 0)
+  			return false
+  		end
+  		x += 1
+  	end
+  	return true
+  end
+end
