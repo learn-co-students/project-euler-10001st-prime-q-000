c03477047cc8201a073f7db73e9572f100017a92
Samuel Lair <lair001@gmail.com>
2016-12-14 00:04:20 -0500

    Done.

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+
+require 'prime'
+
+class Prime
+
+	attr_accessor :input
+
+	def initialize(input)
+		self.input = input
+	end
+
+	def number
+		Prime.first(self.input).last
+	end
+
+end
