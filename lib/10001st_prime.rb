af01b08cc304178c8dae00ae0d5cc45608f5d1cb
shoko furushita <shktkt219@gmail.com>
2017-02-01 16:42:11 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,20 @@
 # Implement your procedural solution here!
+def prime_number_for(nth_element)
+  count = 0
+  number = 0
+  while count != nth_element
+    number += 1
+    count += 1 if prime?(number)
+  end
+  number
+end
+
+def prime?(num)
+  return false if num.zero? || num == 1
+  (2..Math.sqrt(num)).each do |i|
+    if num % i == 0
+      return false
+    end
+  end
+  true
+end
