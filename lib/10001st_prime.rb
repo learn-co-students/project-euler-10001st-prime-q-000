80abc72266dcced9aa067ee9d0b7fe71a534dc8a
SiamKing <thesiameseelectric@gmail.com>
2017-03-22 10:13:35 -0700

    primed and ready

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,27 @@
-# Implement your procedural solution here!
+def prime_number_for(num)
+  primes = []
+  i = 2
+  n = 0
+  loop do
+    if isPrime?(i)
+      primes << i
+      n += 1
+    end
+    break if n == num
+    i += 1
+  end
+  primes.last
+end
+
+def isPrime?(num)
+  prime = num
+  bool = true
+  return false if num.even? && num != 2
+  (2..Math.sqrt(num).to_i).each do |i|
+    if num % i == 0
+      bool = false
+      break
+    end
+  end
+  bool
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,34 @@
-# Implement your object-oriented solution here!
+class Prime
+
+  def initialize(num)
+    @num = num
+  end
+
+  def number
+    primes = []
+    i = 2
+    n = 0
+    loop do
+      if isPrime?(i)
+        primes << i
+        n += 1
+      end
+      break if n == @num
+      i += 1
+    end
+    primes.last
+  end
+
+  def isPrime?(num)
+    prime = num
+    bool = true
+    return false if num.even? && num != 2
+    (2..Math.sqrt(num).to_i).each do |i|
+      if num % i == 0
+        bool = false
+        break
+      end
+    end
+    bool
+  end
+end
