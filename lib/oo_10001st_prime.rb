872c10224e6e5c33ce189aa0b3794130c16b107b
mirandalily <mirandaraymond@me.com>
2017-06-28 12:33:30 -0700

    class prime filling out

--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,10 +1,16 @@
 # Implement your object-oriented solution here!
 class Prime
 
-  def initialize()
-  end
-
-  def number
+  def number(num)
+    counter = 0
+    i = 2
+    while counter < num
+      if i.prime?
+        counter += 1
+      end
+      i + 1 unless counter == num
+    end
+    return i
   end
 
 end
