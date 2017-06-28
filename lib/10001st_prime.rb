e164466726063fb44e6d6a6bac8a7fbdbc297fff
mirandalily <mirandaraymond@me.com>
2017-06-28 12:34:38 -0700

    filled out 10001st_prime, not working

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,3 +1,12 @@
 # Implement your procedural solution here!
 def prime_number_for(num)
+  counter = 0
+  i = 2
+  while counter < num
+    if i.prime?
+      counter += 1
+    end
+    i + 1 unless counter == num
+  end
+  return i
 end
