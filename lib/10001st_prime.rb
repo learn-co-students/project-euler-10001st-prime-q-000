a8ed94de9dd44b2e8edf9fd5052728c5b0459cc3
mirandalily <mirandaraymond@me.com>
2017-06-28 12:31:44 -0700

    set up

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,3 @@
 # Implement your procedural solution here!
+def prime_number_for(num)
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,10 @@
 # Implement your object-oriented solution here!
+class Prime
+
+  def initialize()
+  end
+
+  def number
+  end
+
+end
